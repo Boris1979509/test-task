92348feabc33d239def7d059846b2fb6b807669c
Boris <bobabonanadz@mail.ru>
2022-04-03 14:49:11 +0300

    Create Auth

---
+++ frontend/src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from "vuex";
+import auth from "@/store/modules/auth";
+import { authGuard } from "@/guards/auth.guards";
+import notifications from "@/store/modules/notifications";
+
+const store = createStore({
+  modules: {
+    auth,
+    notifications,
+  },
+});
+/** Auth user */
+await store.dispatch("setLoggedIn");
+authGuard(store);
+
+export default store;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+  return defineConfig({
+    base: process.env.NODE_ENV === "production" ? "/test-task/" : "/",
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@/": `${path.resolve(__dirname, "src")}/`,
+      },
+    },
+    server: {
+      port: process.env.VITE_PORT,
+      host: true,
+    },
+  });
+};
